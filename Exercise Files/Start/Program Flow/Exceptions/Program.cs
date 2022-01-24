11584d522568a504cd0aa1af563f8a1b89d4f526
Gaven Rudiak <gavenpaul12@gmail.com>
2022-01-23 20:36:42 -0500

    Exceptions

--- Exercise Files/Start/Program Flow/Exceptions/Program.cs
+++ Exercise Files/Start/Program Flow/Exceptions/Program.cs
@@ -6,13 +6,30 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
-            int x = 100;
+            int x = 10000;
             int y = 10;
             int result;
 
             // TODO: try-catch expressions make error checking easier
-            result = x / y;
+            try{
+                result = x / y;
+                if (x > 1000){
+                    throw new ArgumentOutOfRangeException("x", "x has to be 1000 or less");
+                }
             Console.WriteLine("The result is: {0}", result);
+            }
+            catch (DivideByZeroException e){
+                Console.WriteLine("Uh Oh!");
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e){
+                Console.WriteLine("Sorry, 1000 is the limit");
+                Console.WriteLine(e.Message);
+            }
+            finally{
+                    Console.WriteLine("this code always runs");
+            }
+
         }
     }
 }
